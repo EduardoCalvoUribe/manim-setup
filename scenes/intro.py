b42274ec0c1acdc5e2169f9d7750d537c8f9c0f2
Eduardo Calvo <calvouribe.eduardo@gmail.com>
2026-06-09 15:43:01 +0200

    test

--- scenes/intro.py
+++ scenes/intro.py
@@ -3,8 +3,8 @@ from manim import *
 
 class Intro(Scene):
     def construct(self):
-        title = Text("Master Thesis", font_size=48)
-        subtitle = Text("Your Name", font_size=32, color=GRAY)
+        title = Text("A Mazing Swarm", font_size=48)
+        subtitle = Text("test", font_size=32, color=GRAY)
         subtitle.next_to(title, DOWN, buff=0.5)
 
         self.play(Write(title))
